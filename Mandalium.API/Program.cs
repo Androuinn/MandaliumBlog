c5801b0d358f33e87e5e388980d2114f932aa5bd
Androuinn <tugay.mandal@gmail.com>
2021-02-07 16:01:56 +0300

    migrations

--- Mandalium.API/Program.cs
+++ Mandalium.API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mandalium.Core.Context;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
@@ -18,7 +21,16 @@ namespace Mandalium.API
             try
             {
                 logger.Debug("init main");
-                CreateHostBuilder(args).Build().Run();
+                var host = CreateHostBuilder(args).Build();
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    services.GetRequiredService<DataContext>().Database.MigrateAsync(); 
+                }
+
+
+                host.Run();
+
             }
             catch (Exception ex)
             {
