be2700aaf0e1fd628fb1b34635e627896c2f3100
TUGAY MANDAL <tugay.mandal@gmail.com>
2021-05-09 16:24:00 +0300

    Update LanguageResource.Designer.cs

--- Mandalium.API/App_GlobalResources/LanguageResource.Designer.cs
+++ Mandalium.API/App_GlobalResources/LanguageResource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Mandalium.Resources {
+namespace Mandalium.API.App_GlobalResources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Mandalium.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mandalium.Resources.LanguageResource", typeof(LanguageResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mandalium.API.App_GlobalResources.LanguageResource", typeof(LanguageResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
